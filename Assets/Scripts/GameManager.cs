e91906d90d9bf221052e68556830ce5d971273dd
XydranChaos <78708475+XydranChaos@users.noreply.github.com>
2021-05-23 16:26:21 -0700

    Unit 5 Lesson A

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private const float spawnRate = 2.0f;
+    public List<GameObject> prefabs;
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            Instantiate(prefabs[Random.Range(0, prefabs.Count)]);
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
